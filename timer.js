1ec088b9d3c2248fb0832cbf382870b2e2ba3a4e
CocoMuffy <cocomuffy15@gmail.com>
2025-07-24 15:02:19 -0400

    added more variables

--- timer.js
+++ timer.js
@@ -7,10 +7,20 @@ function preLoad(){
 
 }
 function setup(){
-  setInterval(timerMaker)
-  startButton = new SpeechRecognitionResult()
+    createCanvas(window.InnerWidth,window.innerHeight);
+    background(255,0,0);
+    setInterval(timerMaker);
+    startButton = new Sprite(200,200,window.InnerWidth/4,window.innerHeight/2);
+    startButton.color = (0,0,255);
+    startButton.text = "Start";
+    pauseButton = new Sprite(200,200, window.InnerWidth/2, window.innerHeight/2);
+    pauseButton.color = (0,255,0);
+    pauseButton.text = "Pause";
+    timerText = new Sprite(800,400,window.InnderWidth/2, window.innerHeight/2);
 }
 function draw(){
+    startButton.pos = {x:200, y:200};
+    pauseButton.pos = 
 
 }
 function timerMaker(){
