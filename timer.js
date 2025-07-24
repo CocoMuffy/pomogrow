74282c83a328168da43095f451b798686b398e03
CocoMuffy <cocomuffy15@gmail.com>
2025-07-24 14:43:29 -0400

    first commit (establish)

---
+++ timer.js
@@ -0,0 +1,18 @@
+let timerVal;
+let startButton;
+let pauseButton;
+let timerText;
+
+function preLoad(){
+
+}
+function setup(){
+  setInterval(timerMaker)
+  startButton = new SpeechRecognitionResult()
+}
+function draw(){
+
+}
+function timerMaker(){
+
+}
