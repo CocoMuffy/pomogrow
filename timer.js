62fa203b73df189dc9fe3397ba95723deb5067bc
CocoMuffy <cocomuffy15@gmail.com>
2025-07-24 22:28:00 -0400

    commiting to save. fixing this in the morning.

--- timer.js
+++ timer.js
@@ -1,47 +1,13 @@
-let timerValWork = 1500;
-let timerValShortB = 300;
-let timerValLongB = 600;
-let startButton;
-let pauseButton;
-let timerText;
-let pomoSection;
-let workState = true;
+  const minutes = document.getElementById("minutes");
+  const seconds = document.getElementById("seconds");
 
-function preLoad(){
+let targetTime = getTime()+1500;
+let startTime = getTime()
 
-}
-function setup(){
-    createCanvas(1920,1080);
-    setInterval(timerMakerW,1000);
-    setInterval(timerMakerSB,1000);
-    setInterval(timerMakerLB,1000);
-    startButton = new Sprite(200,200,window.InnerWidth/4,window.innerHeight/2);
-    startButton.color = (0,0,255);
-    startButton.text = "Start";
-    pauseButton = new Sprite(200,200, window.InnerWidth/2, window.innerHeight/2);
-    pauseButton.color = (0,255,0);
-    pauseButton.text = "Pause";
-    timerText = new Sprite(800,400,window.InnderWidth/2, window.innerHeight/2);
-    pomoSection = 1;
-    timerValWork = 1500;
-    timerValShortB = 300;
-    timerValLongB = 600;
-    workState= true;
-}
-function draw(){
-    background(255,0,0);
-    startButton.pos = {x:200, y:200};
-    pauseButton.pos = {x:800, y:200};
-    timerText.pos = {x:200, y:100};
-    
-    if (startButton.mouse.presses() && pomoSection == 1){
-    setInterval(timerMakerW, 1000);
-    workState= true;
-    }   
+function timer(){
+    const currentDistance = targetTime - startTime
 
-}
+    const minutes = Math.floor(currentDistance/1000/60);
+    const seconds = Math.floor(distance/1000)%60;
 
-function timerMakerW(){
-    timerValWork = 1500;
-    timerValWork--;
 }
